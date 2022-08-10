d54a0f1d3dade5ba8a9184607e875db2401a022a
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-10 13:26:46 -0700

    commit tree 820e236bb49d88bbdafe54905a5749fb3190196a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array
+    array.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms speakers
+
+    speakers.map {|person| "Hello, #{person}! You'll be assigned to room #{speakers.length}!"}
+end
